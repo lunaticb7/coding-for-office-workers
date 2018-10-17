5b0cd64cf934eaf1f607d1c133ebb43b1c93503f
taek nam <lunaticb7@gmail.com>
2018-10-17 23:18:01 +0900

    django

--- week02-django/src/blog/views.py
+++ week02-django/src/blog/views.py
@@ -1,7 +1,22 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.shortcuts import render
-
+# from random import randint
+from .models import Article
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the blog index.")
+    #random_number = randint(1, 10)
+    #return HttpResponse("Hello, world. You're at the blog index.")
+    # return HttpResponse("Hello, world. {}".format(random_number))
+    # name = "marco"
+    # return render(request, "index.html", { "name" : name })
+    article_list = Article.objects.all()
+    # Article.objects.create(
+    #     title="hello",
+    #     contents="this is test",
+    #     view_count=0
+    # )
+    ctx ={
+        "article_list": article_list
+    }
+    return render(request, "index.html", ctx)
